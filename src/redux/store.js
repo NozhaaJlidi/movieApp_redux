63cf56d2a615146b7f1a45ce6aee45613983bda8
Nozha Jlidi <108417286+NozhaaJlidi@users.noreply.github.com>
2022-09-19 13:03:26 +0100

    first commit

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit'
+import movieSlice from './slices/movieSlice'
+
+
+export const store = configureStore({
+  reducer: {
+    movie: movieSlice, 
+  },
+})
+
+
+export default store ;
